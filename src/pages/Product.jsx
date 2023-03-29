73652a83b133997ed816cff41001a0cbfa7d6468
Scott911228 <scott911228@gmail.com>
2023-03-29 23:56:55 +0800

    hihi

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import{ useParams } from 'react-router-dom';
-import Header from "../Components/Header"
+import Header from "../components/Header"
 import Footer from "../components/Footer"
 
 function Product(){
