72b414b038e043ab20f13e4422c0270b48ae2886
Scott911228 <scott911228@gmail.com>
2023-04-10 18:03:03 +0800

    badge count

--- src/Components/CartSummary/index.jsx
+++ src/Components/CartSummary/index.jsx
@@ -19,7 +19,7 @@ export default function CartSummary() {
   return (
     <>
       <nav onClick={toggleOpen} className={styles.cartSummary} >
-        <Badge count={count} color="yellow"  style={{ fontWeight:'bold' , fontSize:'0.8rem' }}>
+        <Badge count={count} color="yellow"  style={{ width:'28px',height:'28px',fontWeight:'bold' , fontSize:'1rem' }}>
           <FontAwesomeIcon icon={faCartShopping} className="carticon" size="2xl" />
         </Badge>
       </nav>
